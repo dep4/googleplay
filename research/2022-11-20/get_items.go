6b3a016309e3c10d8c1a3a3cee24bf0348bd2a5d
Zombo <srpen6@gmail.com>
2022-11-20 20:43:22 -0600

    +var checkin_body = protobuf.Message{

--- research/2022-11-20/get_items.go
+++ research/2022-11-20/get_items.go
@@ -4,7 +4,6 @@ import (
    "bytes"
    "encoding/base64"
    "fmt"
-   "github.com/89z/googleplay"
    "github.com/89z/rosso/http"
    "github.com/89z/rosso/protobuf"
    "io"
@@ -12,6 +11,131 @@ import (
    "strconv"
    "time"
 )
+
+var checkin_body = protobuf.Message{
+   4: protobuf.Message{ // checkin
+      1: protobuf.Message{ // build
+         10: protobuf.Varint(28),
+      },
+      18: protobuf.Varint(1), // voiceCapable
+   },
+   14: protobuf.Varint(3),
+   18: protobuf.Message{ // deviceConfiguration
+      1: protobuf.Varint(0),
+      2: protobuf.Varint(0),
+      3: protobuf.Varint(0),
+      4: protobuf.Varint(0),
+      5: protobuf.Varint(0),
+      6: protobuf.Varint(0),
+      7: protobuf.Varint(0),
+      8: protobuf.Varint(0xF_FFFF),
+      9: protobuf.Slice[protobuf.String]{
+         "android.test.runner",
+         "global-miui11-empty.jar",
+         "org.apache.http.legacy",
+      },
+      11: protobuf.String("x86"),
+      15: protobuf.Slice[protobuf.String]{
+         "GL_OES_compressed_ETC1_RGB8_texture",
+         "GL_KHR_texture_compression_astc_ldr",
+      },
+      26: protobuf.Slice[protobuf.Message]{
+         protobuf.Message{
+            1: protobuf.String("android.hardware.camera.autofocus"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.camera.front"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.camera"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.location.network"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.location.gps"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.location"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.microphone"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.screen.landscape"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.screen.portrait"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.sensor.accelerometer"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.telephony"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.touchscreen"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.touchscreen.multitouch"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.usb.host"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.hardware.wifi"),
+         },
+         protobuf.Message{
+            1: protobuf.String("android.software.device_admin"),
+         },
+      },
+   },
+}
+
+func checkin() (string, error) {
+   req, err := http.NewRequest(
+      "POST", "https://android.googleapis.com/checkin",
+      bytes.NewReader(checkin_body.Marshal()),
+   )
+   if err != nil {
+      return "", err
+   }
+   req.Header.Set("Content-Type", "application/x-protobuffer")
+   res, err := client.Do(req)
+   if err != nil {
+      return "", err
+   }
+   defer res.Body.Close()
+   body, err := io.ReadAll(res.Body)
+   if err != nil {
+      return "", err
+   }
+   message, err := protobuf.Unmarshal(body)
+   if err != nil {
+      return "", err
+   }
+   id, err := message.Get_Fixed64(7)
+   if err != nil {
+      return "", err
+   }
+   return strconv.FormatUint(id, 16), nil
+}
+
+func sync(device string) error {
+   req := new(http.Request)
+   req.Body = io.NopCloser(bytes.NewReader(sync_body.Marshal()))
+   req.Header = make(http.Header)
+   req.Method = "POST"
+   req.URL = new(url.URL)
+   req.URL.Host = "play-fe.googleapis.com"
+   req.URL.Path = "/fdfe/sync"
+   req.URL.Scheme = "https"
+   req.Header["X-Dfe-Device-Id"] = []string{device}
+   if _, err := client.Do(req); err != nil {
+      return err
+   }
+   return nil
+}
 
 func main() {
    device, err := checkin()
@@ -63,48 +187,11 @@ func main() {
    }
 }
 
-func sync(device string) error {
-   req := new(http.Request)
-   req.Body = io.NopCloser(bytes.NewReader(body.Marshal()))
-   req.Header = make(http.Header)
-   req.Method = "POST"
-   req.URL = new(url.URL)
-   req.URL.Host = "play-fe.googleapis.com"
-   req.URL.Path = "/fdfe/sync"
-   req.URL.Scheme = "https"
-   req.Header["X-Dfe-Device-Id"] = []string{device}
-   if _, err := client.Do(req); err != nil {
-      return err
-   }
-   return nil
-}
-
 const sleep = 16 * time.Second
 
 var client = http.Default_Client
 
-func checkin() (string, error) {
-   res, err := googleplay.Phone.Checkin("x86")
-   if err != nil {
-      return "", err
-   }
-   defer res.Body.Close()
-   body, err := io.ReadAll(res.Body)
-   if err != nil {
-      return "", err
-   }
-   message, err := protobuf.Unmarshal(body)
-   if err != nil {
-      return "", err
-   }
-   id, err := googleplay.Device{message}.ID()
-   if err != nil {
-      return "", err
-   }
-   return strconv.FormatUint(id, 16), nil
-}
-
-var body = protobuf.Message{
+var sync_body = protobuf.Message{
    1: protobuf.Slice[protobuf.Message]{
       protobuf.Message{
          10: protobuf.Message{
